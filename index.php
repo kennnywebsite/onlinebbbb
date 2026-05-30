bba1c536bb98301119ba11642978d57b7aa2ef4d
Gabriel Otemu <otemugabriel@gmail.com>
2026-05-30 09:18:00 +0100

    newest update

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+putenv('APP_STORAGE=/var/www/html/storage');
+
 /*
 |--------------------------------------------------------------------------
 | Check If Application Is Under Maintenance
