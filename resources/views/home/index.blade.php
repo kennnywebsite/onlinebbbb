990c6bbad9057d060680fac4edaa0917baf668a3
Gabriel Otemu <otemugabriel@gmail.com>
2026-07-20 20:34:46 +0100

    neww file

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -732,7 +732,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/6a5e61302cd0841d481824cb/1ju0ana3j';
+s1.src='https://embed.tawk.to/6a5e77471c52dc1d4c7ee99e/1ju0g3sto';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
